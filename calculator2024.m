3e38c43712f6f3ce9ffe5630485f4517c1744ff5
Åke Stener <astener@kth.se>
2023-11-17 17:31:11 +0100

    Adding skidpad and acceleration event with GUI

---
+++ calculator2024.m
@@ -0,0 +1,299 @@
+%##########################################################################
+%###################-FSG SCORE CALCULATOR 2024-############################
+%##########################################################################
+close all
+clearvars
+clc
+%==========================================================================
+%-----------------------MENU FOR EVENT GROUP-------------------------------
+%--------------------------------------------------------------------------
+disp('FSG2024 Score Calculator')
+
+msg = "Choose type of test";
+options = ["Manual (M)" "Driverless (DV)" "Driverless Cup (DC)"];
+eventGroupChoice = menu(msg,options);
+
+mEventGroup             = 1;
+dvEventGroup            = 2;
+dcEventGroup            = 3;
+%==========================================================================
+%-----------------------MENU FOR MANUAL EVENTS-----------------------------
+%--------------------------------------------------------------------------
+if eventGroupChoice == mEventGroup
+    disp('FSG2024 Score Calculator')
+
+    msg = "Choose type of test";
+    options = ["Skidpad" "Acceleration" "xxx" "xxx" "xxx"];
+    eventChoice = menu(msg,options);
+
+    eventMSkidpad         = 1;
+    eventMAccel           = 2;
+    xxx                   = 3;
+    xxx                   = 4;
+    xxx                   = 5;
+end
+%==========================================================================
+%------------------------MENU FOR DV EVENTS--------------------------------
+%--------------------------------------------------------------------------
+if eventGroupChoice == dvEventGroup
+    disp('FSG2024 Score Calculator')
+
+    msg = "Choose type of test";
+    options = ["Skidpad" "Acceleration" "xxx" "xxx" "xxx"];
+    eventChoice = menu(msg,options);
+
+    eventDVSkidpad            = 1;
+    eventDVAccel              = 2;
+    xxx                       = 3;
+    xxx                       = 4;
+    xxx                       = 5;
+end
+%==========================================================================
+%---------------------------MENU FOR DC EVENTS-----------------------------
+%--------------------------------------------------------------------------
+if eventGroupChoice == dcEventGroup
+    disp('FSG2024 Score Calculator')
+
+    msg = "Choose type of test";
+    options = ["Skidpad" "Acceleration" "xxx" "xxx" "xxx"];
+    eventChoice = menu(msg,options);
+
+    eventDCSkidpad               = 1;
+    eventDCAccel                 = 2;
+    xxx                          = 3;
+    xxx                          = 4;
+    xxx                          = 5;
+end
+%==========================================================================
+%--------------GUI USERINPUT FOR MANUAL- AND DC EVENTS---------------------
+%--------------------------------------------------------------------------
+if eventGroupChoice ~= dvEventGroup
+   prompt = {'Enter your time: ', 'Enter best time of event: ',...
+       'Enter how many DOO´s', 'Enter how many OC´s', 'Enter how many USS´s',...
+       'Enter how many DOO´s the best team got'};
+   dlgtitle = 'Event input';
+   fieldsize = [1 45; 1 45; 1 45; 1 45; 1 45; 1 45;];
+   defaultinput = {'','', '0', '0', '0','0'};
+    
+   userInput = inputdlg(prompt,dlgtitle,fieldsize,defaultinput);
+    
+   yourTeamTime = str2double(userInput{1});
+    
+   bestTeamTime = str2double(userInput{2});
+
+   penaltyDOO = str2double(userInput{3});
+
+   penaltyOC = str2double(userInput{4});
+
+   penaltyUSS = str2double(userInput{5});
+
+   bestTeamPenaltyDOO = str2double(userInput{6});
+%==========================================================================
+%------------------GUI USERINPUT FOR DV EVENTS-----------------------------
+%--------------------------------------------------------------------------
+elseif eventGroupChoice == dvEventGroup
+    prompt = {'Enter your team´s best time (without penalties)', 'Enter your team´s ranking: ',...
+            'Enter number of team´s who finished atleast one DV run without DNF or DQ: '};
+    dlgtitle = 'Event input';
+    fieldsize = [1 45; 1 45; 1 45;];
+    defaultinput = {'','', ''};
+    
+    userInput = inputdlg(prompt,dlgtitle,fieldsize,defaultinput);
+
+    yourTeamTime = str2double(userInput{1});
+    
+    yourTeamRanking = str2double(userInput{2});
+    
+    numberTeams = str2double(userInput{3});
+end
+%==========================================================================
+%---------------------------CALL TO FUNCTIONS------------------------------
+%--------------------------------------------------------------------------
+%---THE TRY STATEMENT'S ONLY USE IS SO THAT WE CAN HAVE ALL THE CALLS TO--- 
+%---FUNCTIONS AT THE SAME PLACE--------------------------------------------
+%--------------------------------------------------------------------------
+%Call to manual event functions
+try if eventChoice == eventMSkidpad
+    eventScores = mSkidpadEventScore(yourTeamTime, bestTeamTime, penaltyDOO, penaltyOC,...
+        penaltyUSS, bestTeamPenaltyDOO);
+
+    elseif eventChoice == eventMAccel
+        eventScores = mAccelEventScore(yourTeamTime, bestTeamTime, penaltyDOO, penaltyOC,...
+            penaltyUSS, bestTeamPenaltyDOO);
+
+    end
+end
+
+%Call to DV event functions
+try if eventChoice == eventDVSkidpad
+    eventScores = dvSkidpadEventScore(yourTeamTime, yourTeamRanking, numberTeams);
+
+    elseif eventChoice == eventDVAccel
+    eventScores = dvAccelEventScore(yourTeamTime, yourTeamRanking, numberTeams);
+    end
+end
+
+%Call to DC event functions
+try if eventChoice == eventDCSkidpad
+    eventScores = dcSkidpadEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+        penaltyOC, penaltyUSS, bestTeamPenaltyDOO);
+
+    elseif eventChoice == eventDCAccel
+        eventScores = dcAccelEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+            penaltyOC, penaltyUSS, bestTeamPenaltyDOO);
+    end
+end
+%==========================================================================
+%----------------------------PRINTS RESULTS--------------------------------
+disp(round(eventScores,2))
+%==========================================================================
+%---------------------------FUNCTIONS FOR MANUAL---------------------------
+%--------------------------------------------------------------------------
+%----------------------------Manual skidpad--------------------------------
+function [score] = mSkidpadEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+    penaltyOC, penaltyUSS, bestTeamPenaltyDOO)
+    % takes input for a team's time and the best team's time, penalties and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 50;
+
+    if penaltyOC > 0 || penaltyUSS > 0
+        score = 0;
+
+    elseif yourTeamTime + 0.2*penaltyDOO < bestTeamTime + 0.2*bestTeamPenaltyDOO
+        score = maxPoints;
+
+    elseif penaltyOC == 0 && penaltyUSS == 0
+
+        bestTeamTimeFactor = (bestTeamTime + 0.2*bestTeamPenaltyDOO)* 1.25;
+
+        score = 0.95*maxPoints*(((bestTeamTimeFactor / (yourTeamTime + 0.2*penaltyDOO))^2 - 1)...
+                / 0.5625) + 0.05*maxPoints;
+
+        if score <= 0.05*maxPoints
+            score = 0.05*maxPoints;
+        end
+    end
+end
+
+%-------------------------Manual Acceleration------------------------------
+function [score] = mAccelEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+    penaltyOC, penaltyUSS, bestTeamPenaltyDOO)
+    % takes input for a team's time and the best team's time, penalties and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 50;
+
+    if penaltyOC > 0 || penaltyUSS > 0
+        score = 0;
+
+    elseif yourTeamTime + 2*penaltyDOO < bestTeamTime + 2*bestTeamPenaltyDOO
+        score = maxPoints;
+
+    elseif penaltyOC == 0 && penaltyUSS == 0
+
+        bestTeamTimeFactor = (bestTeamTime + 2*bestTeamPenaltyDOO)* 1.5;
+
+        score = 0.95*maxPoints*((bestTeamTimeFactor / (yourTeamTime + 2*penaltyDOO) - 1)...
+                / 0.5) + 0.05*maxPoints;
+
+        if score <= 0.05*maxPoints
+            score = 0.05*maxPoints;
+        end
+    end
+end
+%==========================================================================
+%----------------------------FUNCTIONS FOR DV------------------------------
+%--------------------------------------------------------------------------
+%------------------------------DV Skidpad----------------------------------
+function [score] = dvSkidpadEventScore(yourTeamTime, yourTeamRanking, numberTeams)
+    % takes input for a team's time, your ranking and the number of team's
+    % with atleast one manual or DV run without DNF or DQ 
+    % and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 75;
+    
+    if round(yourTeamTime,3) <= 25
+        score = maxPoints*((numberTeams+1-yourTeamRanking)/numberTeams);
+
+        if score < 0
+            score = 0;
+        end
+
+    elseif round(yourTeamTime,3) > 25
+        score = 0;
+    end
+end
+
+%-----------------------------DV Acceleration------------------------------
+function [score] = dvAccelEventScore(yourTeamTime, yourTeamRanking, numberTeams)
+    % takes input for a team's time, your ranking and the number of team's
+    % with atleast one manual or DV run without DNF or DQ 
+    % and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 75;
+    
+    if round(yourTeamTime,3) <= 25
+        score = maxPoints*((numberTeams+1-yourTeamRanking)/numberTeams);
+
+        if score < 0
+            score = 0;
+        end
+
+    elseif round(yourTeamTime,3) > 25
+        score = 0;
+    end
+end
+
+%==========================================================================
+%--------------------------FUNCTIONS FOR DC--------------------------------
+%--------------------------------------------------------------------------
+%-----------------------------DC Skidpad-----------------------------------
+function [score] = dcSkidpadEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+    penaltyOC, penaltyUSS, bestTeamPenaltyDOO)
+    % takes input for a team's best time and the best team's time, penalties and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 75;
+
+    if penaltyOC > 0 || penaltyUSS > 0
+        score = 0;
+
+    elseif yourTeamTime + 0.2*penaltyDOO < bestTeamTime + 0.2*bestTeamPenaltyDOO
+        score = maxPoints;
+
+    elseif penaltyOC == 0 && penaltyUSS == 0
+    
+        bestTeamTimeFactor = (bestTeamTime + 0.2*bestTeamPenaltyDOO) * 1.5;
+
+        score = 0.95*maxPoints*(((bestTeamTimeFactor / (yourTeamTime + 0.2*penaltyDOO))^2 - 1)...
+            / 1.25) + 0.05*maxPoints;
+
+        if score <= 0.05*maxPoints
+            score = 0.05*maxPoints;
+        end
+    end
+end
+
+%-----------------------------DC Acceleration------------------------------
+function [score] = dcAccelEventScore(yourTeamTime, bestTeamTime, penaltyDOO,...
+    penaltyOC, penaltyUSS, bestTeamPenaltyDOO)
+    % takes input for a team's time and the best team's time, penalties and returns a score
+    % uses FSG2024 Rules
+    maxPoints = 75;
+
+    if penaltyOC > 0 || penaltyUSS > 0
+        score = 0;
+
+    elseif yourTeamTime + 2*penaltyDOO < bestTeamTime + 2*bestTeamPenaltyDOO
+        score = maxPoints;
+
+    elseif penaltyOC == 0 && penaltyUSS == 0
+
+        bestTeamTimeFactor = (bestTeamTime + 2*bestTeamPenaltyDOO)*2;
+
+        score = 0.95*maxPoints*(bestTeamTimeFactor / (yourTeamTime + 2*penaltyDOO) - 1)... 
+            + 0.05*maxPoints;
+
+        if score <= 0.05*maxPoints
+            score = 0.05*maxPoints;
+        end
+    end
+end
